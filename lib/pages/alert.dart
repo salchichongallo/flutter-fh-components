53f001f32f6376cd06f9ec9e5ac293558cd50543
Jimmy Murillo <salchichongallo1997@gmail.com>
2020-07-12 09:51:23 -0500

    add alert dialog demo

--- lib/pages/alert.dart
+++ lib/pages/alert.dart
@@ -5,6 +5,48 @@ class AlertPage extends StatelessWidget {
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(title: Text('Alert Page')),
+      body: Center(
+        child: RaisedButton(
+          child: Text('SHOW ALERT'),
+          onPressed: () {
+            _showAlert(context);
+          },
+          shape: StadiumBorder(),
+        ),
+      ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: _buildDemoDialog,
+    );
+  }
+
+  AlertDialog _buildDemoDialog(BuildContext context) {
+    return AlertDialog(
+      title: Text('Title'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('This is the content...'),
+          FlutterLogo(size: 100),
+        ],
+      ),
+      actions: [
+        ButtonBar(
+          children: [
+            FlatButton(
+                child: Text('CANCEL'),
+                onPressed: () => Navigator.of(context).pop()),
+            FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop()),
+          ],
+        ),
+      ],
     );
   }
 }
